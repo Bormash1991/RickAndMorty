f20b1ad430c5971c935eeb46d28838ba3d33bc2c
Bormash1991 <53615723+Bormash1991@users.noreply.github.com>
2023-03-17 23:48:21 +0200

    fixed bugs

--- src/app/client/client-routing.module.ts
+++ src/app/client/client-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientComponent } from './client.component';
 import { CharactersListComponent } from './characters-list/characters-list.component';
--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ClientRoutingModule } from './client-routing.module';
 import { ClientComponent } from './client.component';
 import { CharactersListComponent } from './characters-list/characters-list.component';
